8d854edf27507bd157242bfe152b146532ecaafc
thomasdavis <thomasalwyndavis@gmail.com>
2014-07-20 22:50:42 +1000

    wip

--- js/main.js
+++ js/main.js
@@ -203,4 +203,71 @@ $(function() {
 			left: ui.size.width
 		});
 	});
+
+
+	$('#login-modal').modal('show')
+
+	/* Session */
+	var SessionModel = Backbone.Model.extend({
+      
+        urlRoot: '/session',
+        initialize: function () {
+          var that = this;
+          // Hook into jquery
+
+          // Use withCredentials to send the server cookies
+          // The server must allow this through response headers
+          $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+          	if(options.url.indexOf('session') !== -1) {
+          		options.url = 'http://registry.jsonresume.org/' + options.url;
+	            options.xhrFields = {
+	              withCredentials: true
+	            };
+	            // If we have a csrf token send it through with the next request
+	            if(typeof that.get('_csrf') !== 'undefined') {
+	              jqXHR.setRequestHeader('X-CSRF-Token', that.get('_csrf'));
+	            }
+	        }
+          });
+        },
+        login: function(creds) {
+          // Do a POST to /session and send the serialized form creds
+          this.save(creds, {
+             success: function () {}
+          });
+        },
+        logout: function() {
+          // Do a DELETE to /session and clear the clientside data
+          var that = this;
+          this.destroy({
+            success: function (model, resp) {
+              model.clear()
+              model.id = null;
+              // Set auth to false to trigger a change:auth event
+              // The server also returns a new csrf token so that
+              // the user can relogin without refreshing the page
+              that.set({auth: false});//, _csrf: resp._csrf});
+              
+            }
+          });      
+        },
+        getAuth: function(callback) {
+          // getAuth is wrapped around our router
+          // before we start any routers let us see if the user is valid
+          this.fetch({
+              success: callback
+          });
+        }
+      }); 
+      var Session = new SessionModel();
+      Session.getAuth(function (session) {
+      	console.log('auth', session.get('auth'));
+      });
+      $('.login-form').on('submit', function (ev) {
+      	var form = $(ev.currentTarget);
+      	var email = $('.login-email', form).val();
+      	var password = $('.login-password', form).val();
+      	console.log(email,password);
+      	return false;
+      })
 });
